bcde580b038a3402d2c53889938d6f04eeb53ea7
matsuoka-601 <kokoro601@gmail.com>
2022-03-23 11:42:40 +0900

    KDTreeのNNSを実装

--- DataStructure/KDTree.hpp
+++ DataStructure/KDTree.hpp
@@ -12,6 +12,7 @@ public:
     T x, y;
     Point() {}
     Point(int id, T x, T y): id(id), x(x), y(y) {}
+    Point(T x, T y): id(-1), x(x), y(y) {}
 };
 
 template<class T> class KDTree {
@@ -36,11 +37,37 @@ template<class T> class KDTree {
         Node* node = (Node *) malloc(sizeof(Node));
         node->location = mid;
         node->l = build(l, mid, depth + 1);
-        node->r = build(mid + 1, r, depth + 1);
+        node->r = build(mid + 1, r, depth + 1); // skip the median
         return node;
     }
 
+    static inline T distance(Point<T> &p, Point<T> &q) {
+        return (p.x - q.x)*(p.x - q.x) + (p.y - q.y)*(p.y - q.y);
+    }
+
+    void find(Node *v, Point<T> &q, int *guess, T *min_dist, int depth) {
+        if (!v) return;
+        int loc = v->location;
+
+        Point<T> &p = points[loc];
+        T cur_dist = distance(p, q);
+        if (cur_dist < *min_dist) {
+            *guess = loc;
+            *min_dist = cur_dist;
+        }
+
+        Node *nxt = v->l, *other = v->r;
+        if (depth % 2 == 0 && q.x >= p.x) std::swap(nxt, other);
+        if (depth % 2 != 0 && q.y >= p.y) std::swap(nxt, other);
+
+        find(nxt, q, guess, min_dist, depth + 1);
+        
+        if (depth % 2 == 0 && (q.x - p.x)*(q.x - p.x) < *min_dist) find(other, q, guess, min_dist, depth + 1);
+        if (depth % 2 != 0 && (q.y - p.y)*(q.y - p.y) < *min_dist) find(other, q, guess, min_dist, depth + 1);
+    }
+
     void find(Node* v, int sx, int tx, int sy, int ty, int depth, std::vector< Point<T> > &result) {
+        if (!v) return;
         int loc = v->location;
         T x = points[loc].x;
         T y = points[loc].y;
@@ -48,11 +75,11 @@ template<class T> class KDTree {
         if (sx <= x && x <= tx && sy <= y && y <= ty) result.push_back(points[loc]);
 
         if (depth % 2 == 0) {
-            if (v->l != nullptr && sx <= x) find(v->l, sx, tx, sy, ty, depth + 1, result); 
-            if (v->r != nullptr && x <= tx) find(v->r, sx, tx, sy, ty, depth + 1, result); 
+            if (sx <= x) find(v->l, sx, tx, sy, ty, depth + 1, result); 
+            if (x <= tx) find(v->r, sx, tx, sy, ty, depth + 1, result); 
         } else {
-            if (v->l != nullptr && sy <= y) find(v->l, sx, tx, sy, ty, depth + 1, result);
-            if (v->r != nullptr && y <= ty) find(v->r, sx, tx, sy, ty, depth + 1, result);
+            if (sy <= y) find(v->l, sx, tx, sy, ty, depth + 1, result);
+            if (y <= ty) find(v->r, sx, tx, sy, ty, depth + 1, result);
         }
     }
 
@@ -66,8 +93,11 @@ public:
     }
 
     // find the closest point to q and return the id of the point
-    Point<T> find(Point<T> &q) {
-        return NULL; // to implement
+    Point<T> find(Point<T> q) {
+        T min_dist =  std::numeric_limits<T>::max();
+        int guess = -1;
+        find(root, q, &guess, &min_dist, 0);
+        return points[guess]; 
     }
 
     // find points in [sx, tx] x [sy, ty] and append these points to result
