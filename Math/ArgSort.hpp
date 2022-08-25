103579bc4700a419faa666416cc73f999a65b7a9
kokoro601 <kokoro601@gmail.com>
2022-08-25 12:31:03 +0900

    ArgSort.hpp で、usingを用いていたのを削除

--- Math/ArgSort.hpp
+++ Math/ArgSort.hpp
@@ -1,19 +1,18 @@
 #pragma once
 #include <tuple>
 
-using ll = long long;
 
 
-inline int calc_which_half(std::pair<ll, ll> p) {
-    ll x, y;
+inline int calc_which_half(std::pair<long long, long long> p) {
+    long long x, y;
     std::tie(x, y) = p;
     if (y > 0) return 0;
     if (y < 0) return 1;
     return x < 0;
 }
 
-bool argsort_compare(std::pair<ll, ll> p1, std::pair<ll, ll> p2) {
-    ll x1, y1, x2, y2;
+bool argsort_compare(std::pair<long long, long long> p1, std::pair<long long, long long> p2) {
+    long long x1, y1, x2, y2;
     std::tie(x1, y1) = p1;
     std::tie(x2, y2) = p2;
     int h1 = calc_which_half(p1);
